8547840e852553e28b8714c2945db6685b522346
ThirdAttempt <anirudhmabhilash@gmail.com>
2026-06-02 19:35:54 +0530

    Add CORS

--- main.py
+++ main.py
@@ -2,10 +2,23 @@ from fastapi import FastAPI
 from database import Base, engine
 from routers import users, items, bids, auth
 from datetime import datetime, timezone
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 Base.metadata.create_all(engine)
 
 app = FastAPI()
+
+app.add_middleware (
+    CORSMiddleware,
+    allow_origins=[os.getenv("FRONTEND_URL", "*")],
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
 app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(items.router, prefix="/items", tags=["items"])
